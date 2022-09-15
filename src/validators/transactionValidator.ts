a262f59f1a1763a3293aad066c1e26fec68fec5d
ayush191002 <ayush.dhondiyal@locusnine.com>
2022-09-15 12:15:15 +0530

    creating trasvalidator2

--- src/validators/transactionValidator.ts
+++ src/validators/transactionValidator.ts
@@ -1,11 +1,29 @@
 import { Validator } from "fluentvalidation-ts";
+import { ValidationErrors } from "fluentvalidation-ts/dist/ValidationErrors";
+import { runInThisContext } from "vm";
 import { AccountConditions } from "../models/AccountConditions";
 import { TransactionEntry } from '../models/TransactionEntry'
 
-class TransactionValidator extends Validator<AccountConditions>{
+class TransactionValidator extends Validator<AccountConditions> {
     constructor(){
         super();
-        
+    }
+
+    public isValid(instance: any): Boolean {
+        return this.hasValidationErrors(this.validate(instance))
+    }
+
+    private hasValidationErrors(validationErrors: ValidationErrors<any>): boolean {
+        let values = Object.values(validationErrors);
+        return this.checkValidationErrors(values);
+    }
+
+  
+    private checkValidationErrors(validationResponse: any[]): any {
+        if((validationResponse || []).length){
+
+        }
+        return true;
     }
 }
 
